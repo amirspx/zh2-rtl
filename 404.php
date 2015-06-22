711af0468a691fd6d03884b51a995fa694d18e90
Amir <amir.sadeghpour@gmail.com>
2015-06-23 01:21:19 +0430

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+		<h2>شما از زمان گذر کرده‌اید!</h2>
+
+		<div class="post">
+
+			<br/>
+			<center>
+			<!-- <p>دنبال چیزی هستید که وجود ندارد</p> -->
+
+			<!-- <p>نمی‌توانید چیزی که می‌خواهید بیابید</p> -->
+
+			<p>برگردید به <a href="<?php echo get_option('home'); ?>/">گذراندن زمان</a>.</p>
+			</center>
+
+		</div> <!-- /end .post -->
+
+	</div>  <!-- /end .container -->
+
+	<?php wp_footer(); ?>
+
+</body>
+</html>

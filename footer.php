116fe76189683fb8e10e0ebaba7e61b20da9dde1
Amir <amir.sadeghpour@gmail.com>
2015-06-23 01:22:38 +0430

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+		<div class="subscribe">
+				<!-- <a href="http://youremailsubscriptionlink.com">اشتراک ایمیلی</a> -->
+		</div> <!-- /end .subscribe -->
+	
+
+		<div class="footer">
+			<p><a href="/">نام بلاگ</a> :: <a href="/about/">درباره</a> :: <a href="/archives">فهرست</a>
+			</p>
+			<br/>
+			<br/>
+			<p>محتوای این بلاگ، <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">تحت لیسانس کریتیوکامنز</a> و متعلق به <a href="/"><?php $author = get_the_author(); ?></a> است. 
+<br/>
+<a href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img src="/wp-content/themes/zh2/cc-amirsp.png" alt="paste here" /></a>
+			</p>
+
+			</div><!-- /end .footer-->
+
+	</div>  <!-- /end .container -->
+
+	<?php wp_footer(); ?>
+
+</body>
+</html>

c879f5ceb3f88a43a39156819c4ba9e721996b69
Amir <amir.sadeghpour@gmail.com>
2015-06-23 01:23:32 +0430

    Create footer1.php

---
+++ footer1.php
@@ -0,0 +1,16 @@
+		<div class="subscribe">
+			<p>اشتراک: <a href="/feed">فید</a> | <a href="https://twitter.com/">توئیتر</a> | <a href="http://gplus.to/">گوگل‌پلاس</a></p>
+		</div> <!-- /end .subscribe -->
+
+
+		<div class="footer">
+			<p><a href="/"><?php bloginfo('name'); ?></a> :: <a href="/about/">درباره</a> :: <a href="/archives">فهرست</a>
+			</p>
+			</div><!-- /end .footer-->
+
+	</div>  <!-- /end .container -->
+
+	<?php wp_footer(); ?>
+
+</body>
+</html>
